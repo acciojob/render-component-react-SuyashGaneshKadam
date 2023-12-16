d9e041dbd87c1ca75cd31e71c2170dd4472a1819
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-12-16 15:46:37 +0000

    Added a component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Text from "./Text";
 
 const App = () => {
   return (
     <div id="main">
-      I am learning React. My life is getting better
+      <Text/>
     </div>
   )
 }
---
+++ src/components/Text.js
@@ -0,0 +1,11 @@
+import React, { Component, useState } from "react";
+import '../styles/App.css';
+
+const Text = () => {
+    return (
+        <p>I am learning React. My life is getting better</p>
+    )
+}
+
+
+export default Text;
